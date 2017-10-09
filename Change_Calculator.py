2269f17b8210fd08d16608b805eca8a50cc407cd
Alex <halcyonyears@hotmail.com>
2017-10-09 15:49:35 -0700

    removed an unnecessary variable, etc.
    didn't need amount_n, moved print function outside function, and edited comments and had " instead of ' on a print statement.

--- Change_Calculator.py
+++ Change_Calculator.py
@@ -13,22 +13,20 @@ Created on Mon Oct  9 11:29:04 2017
 # Variables
 again = 'yes' #whether the user wants to use the program again
 coin_v = [0.25, 0.10, 0.05, 0.01] #values of quarter, dime, nickle, penny
+coin_n = [0, 0, 0, 0] #holds number of coins needed to make amount
 
 # Function
-def coin(amount_n):
+def coin(amount):
     """
-    amount_n = float, change from user entered purchase
+    amount = float, change from user entered purchase
     
-    breaks down amount_n into coins by subtracting them from amount_n
+    breaks down amount_n into coins by subtracting them from amount
     """
-    coin_n = [0, 0, 0, 0] #holds number of coins needed to make amount
     for i in range(len(coin_v)):
-        while round(amount_n, 2) >= coin_v[i]:
+        while round(amount, 2) >= coin_v[i]:
             coin_n[i] += 1
-            amount_n -= coin_v[i]
-    return print('You need {} quarters, {} dimes, {} nickles, and {} pennies '
-                 'to make ${}.' .format(coin_n[0], coin_n[1], coin_n[2], 
-                           coin_n[3], amount))
+            amount -= coin_v[i]
+    return coin_n
 
 
 # Start of program
@@ -37,10 +35,12 @@ print('This program calculates the change from a purchase and what coins '
 
 while again == 'yes':
     price = float(input('Please enter the price of the item: '))
-    money = float(input("Please enter the amount of money received: "))
+    money = float(input('Please enter the amount of money received: '))
     amount = round(money - price, 2) #change from purchase
-    amount_n = amount #amount used for calculations
-    coin(amount_n)
+    coin(amount)
+    print('You need {} quarters, {} dimes, {} nickles, and {} pennies '
+                 'to make ${}.' .format(coin_n[0], coin_n[1], coin_n[2], 
+                           coin_n[3], amount))
     again = input('Would you like to enter another amount? yes, no: ')
             
 print('Thanks for using my program!')
